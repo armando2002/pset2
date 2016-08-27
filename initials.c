435c008840a0738a00a74c22a143d17975607ee7
Stephen Williams <armando2002@gmail.com>
2016-08-27 22:24:02 +0000

    Adding initials.c

--- initials.c
+++ initials.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include <cs50.h>
-#include <math.h>
+#include <string.h>
+#include <ctype.h>
+
 
 int main(void)
     {
         string name;
         printf("Enter your name: ");
         name = GetString();
-        printf("\n%s\n", name);
+        
+        printf("%c", toupper(name[0]));
+        
+        for (int i = 0, n = strlen(name); i < n; i++)
+        {
+            if (name[i] == ' ' && name[i + 1] != '\0')
+            {
+                printf("%c", toupper(name[i + 1]));
+                i++;
+            }
+        }
+        printf("\n");
     }
     
