0238b937e61f0edf4125fb488d6001b157afa521
Stephen Williams <armando2002@gmail.com>
2016-08-27 19:04:17 +0000

    Initial Commit

---
+++ initials.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main(void)
+    {
+        string name;
+        printf("Enter your name: ");
+        name = GetString();
+        printf("\n%s\n", name);
+    }
+    
